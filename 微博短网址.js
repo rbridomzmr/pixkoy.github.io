3f00f1b38ce11fce74dd10c620f6a6b85b084cec
Pixkoy <pixkoy@gmail.com>
2016-01-19 07:25:40 +0800

    Delete 微博短网址.js

--- 微博短网址.js
+++
@@ -1,1 +0,0 @@
-if(location.href.match(5786724301)){document.write('<style type="text/css">body{background:#333;color:#f69;font-family:Microsoft YaHei Light;font-size:10em;margin:0px auto;text-align:center;}</style></br>'+document.body.innerHTML.match(/(short":")(.{15,25})(",")/)[2]);}else{window.open('http://api.weibo.com/2/short_url/shorten.json?source=5786724301&url_long='+location.href);}
